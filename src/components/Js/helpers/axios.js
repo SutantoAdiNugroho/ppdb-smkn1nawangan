86174c776dc535c21530135dc561a445cb06a714
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2020-07-10 09:52:16 +0700

    [helpers, ] add decrypt token login

--- src/components/Js/helpers/axios.js
+++ src/components/Js/helpers/axios.js
@@ -1,14 +1,18 @@
 import axios from "axios"
+import CryptoJs from "crypto-js"
 
 export const axiosReportsUsers = () => {
     axios.defaults.baseURL = process.env.REACT_APP_API_LOGIN_LIVE
     axios.defaults.headers.common["X-API-KEY"] = 
         process.env.REACT_APP_JWT_SECRET_KEY 
 
-    const token = localStorage.getItem("token")
+    const token = JSON.parse(localStorage.getItem("token"))
+
+    var bytes  = CryptoJs.AES.decrypt(token, process.env.REACT_APP_JWT_SECRET_KEY);
+    var plaintext = bytes.toString(CryptoJs.enc.Utf8);        
         
     if (token) {
-        axios.defaults.headers.common["Authorization"] = `Bearer ${JSON.parse (token)}`
+        axios.defaults.headers.common["Authorization"] = `Bearer ${plaintext}`
     }
     return axios;
 }
--- src/components/Js/helpers/verifyToken.js
+++ src/components/Js/helpers/verifyToken.js
@@ -1,9 +1,13 @@
 import jwt from "jsonwebtoken"
+import CryptoJs from "crypto-js"
 
 export const verify = () => {
-    const token = JSON.parse(localStorage.getItem("token"))
 
-    var decoded = jwt.verify(token, process.env.REACT_APP_JWT_SECRET_KEY)
+    const token = JSON.parse(localStorage.getItem("token"))
+    var bytes  = CryptoJs.AES.decrypt(token, process.env.REACT_APP_JWT_SECRET_KEY);
+    var plaintext = bytes.toString(CryptoJs.enc.Utf8);    
+
+    var decoded = jwt.verify(plaintext, process.env.REACT_APP_JWT_SECRET_KEY)
 
     return decoded;
 }
